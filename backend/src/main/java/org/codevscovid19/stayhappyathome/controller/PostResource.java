1037a310d897eb87863350a09f9fc876c31b709c
Jonas Alder <jonas.alder@gmail.com>
2020-03-27 23:56:49 +0100

    centralize /post on resource

--- backend/src/main/java/org/codevscovid19/stayhappyathome/controller/PostResource.java
+++ backend/src/main/java/org/codevscovid19/stayhappyathome/controller/PostResource.java
@@ -14,35 +14,35 @@ import org.springframework.web.bind.annotation.RestController;
 import java.util.List;
 
 @RestController
-@RequestMapping("/api/v1")
+@RequestMapping("/api/v1/post")
 public class PostResource {
 
-	@GetMapping(path = "/post", produces = "application/json")
+	@GetMapping(path = "", produces = "application/json")
 	public ResponseEntity<List<Post>> getPosts(){
 		return ResponseEntity.ok(List.of(new Post(1)));
 	}
 
-	@PostMapping(path = "/post", produces = "application/json", consumes = "application/json")
+	@PostMapping(path = "", produces = "application/json", consumes = "application/json")
 	public ResponseEntity<Post> createPosts(@RequestBody Post post){
 		return ResponseEntity.ok(post);
 	}
 
-	@GetMapping(path = "/post/{id}", produces = "application/json")
+	@GetMapping(path = "/{id}", produces = "application/json")
 	public ResponseEntity<Post> getPost(@PathVariable("id") Integer id){
 		return ResponseEntity.ok(new Post(id));
 	}
 
-	@PostMapping(path = "/post/{postId}/reply", produces = "application/json", consumes = "application/json")
+	@PostMapping(path = "/{postId}/reply", produces = "application/json", consumes = "application/json")
 	public ResponseEntity<Reply> createReply(@PathVariable("postId") Integer postId, @RequestBody Reply reply){
 		return ResponseEntity.ok(reply);
 	}
 
-	@PostMapping(path = "/post/{postId}/reaction", produces = "application/json", consumes = "application/json")
+	@PostMapping(path = "/{postId}/reaction", produces = "application/json", consumes = "application/json")
 	public ResponseEntity<Reaction> createPostReaction(@PathVariable("postId") Integer postId, @RequestBody Reaction reaction){
 		return ResponseEntity.ok(reaction);
 	}
 
-	@PostMapping(path = "/post/{postId}/reply/{replyId}/reaction", produces = "application/json", consumes = "application/json")
+	@PostMapping(path = "/{postId}/reply/{replyId}/reaction", produces = "application/json", consumes = "application/json")
 	public ResponseEntity<Reaction> createReplyReaction(@PathVariable("postId") Integer postId, @PathVariable("replyId") Integer replyId, @RequestBody Reaction reaction){
 		return ResponseEntity.ok(reaction);
 	}
