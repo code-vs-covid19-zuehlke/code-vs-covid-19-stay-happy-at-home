eff0c7b966d0a1d92c6e106d12cdb0a46e590516
Frei, Jasmin <jasmin.frei@zuehlke.com>
2020-03-27 22:36:39 +0100

    fixed an error that caused an ugly formatting :(

--- frontend/happy_at_home/lib/pages/ContentFeed.dart
+++ frontend/happy_at_home/lib/pages/ContentFeed.dart
@@ -10,23 +10,24 @@ class _ContentFeedState extends State<ContentFeed> {
   Widget build(BuildContext context) {
     return Scaffold(
       body: SafeArea(
-          child: Column(
-        children: <Widget>[
-          Text("Content Feed Page"),
-          FlatButton.icon(
-            onPressed: () {
-              Navigator.pushNamed(context, "/detail");
-            },
-            label: Text(
-              "Go to Content Detail page",
-            ),
-            icon: Icon(
-              Icons.edit,
-              color: Colors.blue,
-            ),
-          )
-        ],
-      )),
+        child: Column(
+          children: <Widget>[
+            Text("Content Feed Page"),
+            FlatButton.icon(
+              onPressed: () {
+                Navigator.pushNamed(context, "/detail");
+              },
+              label: Text(
+                "Go to Content Detail page",
+              ),
+              icon: Icon(
+                Icons.edit,
+                color: Colors.blue,
+              ),
+            )
+          ],
+        ),
+      ),
     );
   }
 }
