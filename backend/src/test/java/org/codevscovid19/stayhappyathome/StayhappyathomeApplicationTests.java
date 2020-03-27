d3d914de30f80f8ee10350cb30391553e1f96a12
ken.iseli@zuehlke.com <ken.iseli@zuehlke.com>
2020-03-27 21:19:13 +0100

    initial backend commit.

---
+++ backend/src/test/java/org/codevscovid19/stayhappyathome/StayhappyathomeApplicationTests.java
@@ -0,0 +1,13 @@
+package org.codevscovid19.stayhappyathome;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StayhappyathomeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
