f0eaa1eb57a1ecd375869dfdc24d73f9fd517c62
Jonas Alder <jonas.alder@gmail.com>
2020-03-27 22:36:36 +0100

    make user resource great again

---
+++ backend/src/main/java/org/codevscovid19/stayhappyathome/controller/UserResource.java
@@ -0,0 +1,22 @@
+package org.codevscovid19.stayhappyathome.controller;
+
+import org.codevscovid19.stayhappyathome.dto.User;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/v1")
+public class UserResource {
+
+	@Autowired
+	public UserResource() {
+	}
+
+	@GetMapping(path = "/user", produces = "application/json")
+	public ResponseEntity<User> getUser(){
+		return ResponseEntity.ok(new User("Hans12"));
+	}
+}
---
+++ backend/src/main/java/org/codevscovid19/stayhappyathome/dto/User.java
@@ -0,0 +1,14 @@
+package org.codevscovid19.stayhappyathome.dto;
+
+public class User {
+
+	private String name;
+
+	public User(String name) {
+		this.name = name;
+	}
+
+	public String getName() {
+		return name;
+	}
+}
