7e6ff1fbdf951cce72e270af555b61eb9b437d3e
Savrim, Tuana <tuana.savrim@zuehlke.com>
2020-03-28 15:48:38 +0100

    check if user exists

--- backend/src/main/java/org/codevscovid19/stayhappyathome/service/PostService.java
+++ backend/src/main/java/org/codevscovid19/stayhappyathome/service/PostService.java
@@ -1,6 +1,7 @@
 package org.codevscovid19.stayhappyathome.service;
 
 import org.codevscovid19.stayhappyathome.entity.Post;
+import org.codevscovid19.stayhappyathome.entity.User;
 import org.springframework.context.annotation.Bean;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Component;
@@ -9,7 +10,7 @@ import java.util.List;
 
 @Component
 public class PostService {
-    public ResponseEntity<List<Post>> getPostsForUser() {
+    public ResponseEntity<List<Post>> getPostsForUser(User user1) {
         return null;
     }
 }
