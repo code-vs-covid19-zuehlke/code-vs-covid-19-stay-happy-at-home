8a460d77e04a321e079874ff24aabffc5794cdee
Jasmin Frei <jasmin.frei@hotmail.com>
2020-03-28 13:11:56 +0100

    added more "functionality" to register user

--- frontend/happy_at_home/lib/pages/Register.dart
+++ frontend/happy_at_home/lib/pages/Register.dart
@@ -1,4 +1,7 @@
+import 'dart:io';
+
 import 'package:flutter/material.dart';
+import 'package:happyathome/widgets/ProfileImgWidget.dart';
 
 class Register extends StatefulWidget {
   @override
@@ -6,26 +9,61 @@ class Register extends StatefulWidget {
 }
 
 class _RegisterState extends State<Register> {
+  File _image;
+
+  // Create a text controller and use it to retrieve the current value
+  // of the TextField.
+  final myController = TextEditingController();
+
+  void onChooseImage(image) {
+    setState(() {
+      _image = image;
+    });
+  }
+
+  void createUser() {
+    var name = myController.text;
+    //Todo: Save Name and Image here
+    Navigator.pushNamed(context, "/feeling");
+  }
+
+  @override
+  void dispose() {
+    // Clean up the controller when the widget is disposed.
+    myController.dispose();
+    super.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: SafeArea(
-        child: Column(
-          children: <Widget>[
-            Text("Register User Page"),
-            FlatButton.icon(
-              onPressed: () {
-                Navigator.pushNamed(context, "/feeling");
-              },
-              label: Text(
-                "Register now",
+        child: Center(
+          child: Column(
+            children: <Widget>[
+              Text("Register User Page"),
+              Padding(
+                padding: const EdgeInsets.all(16.0),
+                child: TextField(
+                  controller: myController,
+                  decoration: InputDecoration(
+                      border: OutlineInputBorder(),
+                      hintText: 'Enter your name'),
+                ),
               ),
-              icon: Icon(
-                Icons.edit,
-                color: Colors.blue,
-              ),
-            )
-          ],
+              ProfileImgWidget(_image, onChooseImage),
+              FlatButton.icon(
+                onPressed: createUser,
+                label: Text(
+                  "Register now",
+                ),
+                icon: Icon(
+                  Icons.edit,
+                  color: Colors.blue,
+                ),
+              )
+            ],
+          ),
         ),
       ),
     );
