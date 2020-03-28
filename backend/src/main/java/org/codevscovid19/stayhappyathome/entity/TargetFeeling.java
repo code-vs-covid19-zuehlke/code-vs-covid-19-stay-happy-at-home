c03e3bff25d7de187e955c9be3a04a3ccd126f1a
Savrim, Tuana <tuana.savrim@zuehlke.com>
2020-03-28 23:58:13 +0100

    Add FeelingMatcherMap

--- backend/src/main/java/org/codevscovid19/stayhappyathome/entity/TargetFeeling.java
+++ backend/src/main/java/org/codevscovid19/stayhappyathome/entity/TargetFeeling.java
@@ -1,5 +1,11 @@
 package org.codevscovid19.stayhappyathome.entity;
 
+import java.util.Set;
+
 public enum TargetFeeling {
   HAPPY, RELAXED, ACCOMPLISHED, INFORMED, ENERGIZED, AWWW, INSPIRED, ENTERTAINED;
+
+  public static Set<TargetFeeling> getAll() {
+    return Set.of(HAPPY, RELAXED, ACCOMPLISHED, INFORMED, ENERGIZED, AWWW, INSPIRED, ENTERTAINED);
+  }
 }
--- backend/src/main/java/org/codevscovid19/stayhappyathome/service/PostService.java
+++ backend/src/main/java/org/codevscovid19/stayhappyathome/service/PostService.java
@@ -6,29 +6,32 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 import java.time.LocalDateTime;
-import java.util.HashSet;
-import java.util.List;
-import java.util.Set;
+import java.util.*;
 
 @Component
 public class PostService {
-    private PostRepository postRepository;
+  private PostRepository postRepository;
+  private FeelingMatcherMap feelingMatcherMap;
 
-    @Autowired
-    public PostService(PostRepository postRepository) {
-        this.postRepository = postRepository;
-    }
+  @Autowired
 
-    public Set<Post> getPostsForUser(User user) {
-        Set<Post> posts = new HashSet<>();
-        FeelingRecord latestFeelingRecord = new FeelingRecord();
-        latestFeelingRecord.setTime(LocalDateTime.now());
-        latestFeelingRecord.setFeelings(List.of(new Feeling(Emoji.DOWNCAST_FACE_WITH_SWEAT)));
-        /*user.getFeelingRecords().stream()
-                .max(Comparator.comparing(FeelingRecord::getTime))
-                .orElseThrow(() -> new IllegalStateException(String.format("User %s has no feelings", user)));*/
-        // latestFeelingRecord.getFeelings().forEach(feeling -> posts.addAll(postRepository.findByFeeling(feeling)));
-        latestFeelingRecord.getFeelings().forEach(feeling -> posts.addAll(postRepository.findAll()));
-        return posts;
-    }
+  public PostService(PostRepository postRepository) {
+    this.postRepository = postRepository;
+  }
+
+  public Set<Post> getPostsForUser(User user) {
+    Set<Post> posts = new HashSet<>();
+    FeelingRecord latestFeelingRecord = user.getFeelingRecords().stream()
+      .max(Comparator.comparing(FeelingRecord::getTime))
+      .orElseThrow(() -> new IllegalStateException(String.format("User %s has no feelings", user)));
+    latestFeelingRecord.getFeelings().forEach(feeling -> posts.addAll(getPostsForFeeling(feeling)));
+    return posts;
+  }
+
+  private Collection<Post> getPostsForFeeling(Feeling feeling) {
+    Collection<Post> postsForFeeling = new HashSet<>();
+    Set<TargetFeeling> targetFeelings = feelingMatcherMap.getEmotionToEmotionMatrix().get(feeling);
+    targetFeelings.forEach(targetFeeling -> postsForFeeling.addAll(postRepository.findByTargetFeeling(targetFeeling)));
+    return postsForFeeling;
+  }
 }
