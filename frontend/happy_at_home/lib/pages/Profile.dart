bd4596badfae4e8e52a658ecb8d54eee8b6a0265
Frei, Jasmin <jasmin.frei@zuehlke.com>
2020-03-28 10:06:52 +0100

    added the "choose image" function

--- frontend/happy_at_home/lib/pages/Profile.dart
+++ frontend/happy_at_home/lib/pages/Profile.dart
@@ -1,10 +1,11 @@
+import 'dart:io';
 import 'dart:math';
 
 import 'package:flutter/material.dart';
 import 'package:happyathome/apis/Backend.dart';
 import 'package:happyathome/models/User.dart';
+import 'package:happyathome/widgets/ProfileImgWidget.dart';
 import 'package:happyathome/widgets/UserWidget.dart';
-
 
 class Profile extends StatefulWidget {
   @override
@@ -13,60 +14,71 @@ class Profile extends StatefulWidget {
 
 class _ProfileState extends State<Profile> {
   Future<User> futureUser;
+  File _image;
 
   @override
   void initState() {
     super.initState();
   }
 
+  void onChooseImage(image) {
+    setState(() {
+      _image = image;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: SafeArea(
+        child: Center(
           child: Column(
-        children: <Widget>[
-          Text("Profile Page"),
-          FlatButton.icon(
-            onPressed: () {
-              Navigator.pushNamed(context, "/feeling");
-            },
-            label: Text(
-              "Go to Feeling page",
-            ),
-            icon: Icon(
-              Icons.edit,
-              color: Colors.blue,
-            ),
+            children: <Widget>[
+              Text("Profile Page"),
+              FlatButton.icon(
+                onPressed: () {
+                  Navigator.pushNamed(context, "/feeling");
+                },
+                label: Text(
+                  "Go to Feeling page",
+                ),
+                icon: Icon(
+                  Icons.edit,
+                  color: Colors.blue,
+                ),
+              ),
+              FlatButton.icon(
+                onPressed: () {
+                  Navigator.pushNamed(context, "/create");
+                },
+                label: Text(
+                  "Create Content",
+                ),
+                icon: Icon(
+                  Icons.edit,
+                  color: Colors.blue,
+                ),
+              ),
+              FlatButton.icon(
+                onPressed: () {
+                  setState(() {
+                    futureUser = Backend.fetchUser(Random().nextInt(5) + 1);
+                  });
+                },
+                label: Text(
+                  "Fetch user",
+                ),
+                icon: Icon(
+                  Icons.file_download,
+                  color: Colors.blue,
+                ),
+              ),
+              UserWidget(futureUser),
+              ProfileImgWidget(_image, onChooseImage),
+            ],
           ),
-          FlatButton.icon(
-            onPressed: () {
-              Navigator.pushNamed(context, "/create");
-            },
-            label: Text(
-              "Create Content",
-            ),
-            icon: Icon(
-              Icons.edit,
-              color: Colors.blue,
-            ),
-          ),
-          FlatButton.icon(
-            onPressed: () {
-              setState(() {
-                futureUser = Backend.fetchUser(Random().nextInt(5)+1);
-              });
-            },
-            label: Text(
-              "Fetch user",
-            ),
-            icon: Icon(
-              Icons.file_download,
-              color: Colors.blue,
-            ),
-          ),
-          UserWidget(futureUser)
-        ],
-      )),
+        ),
+      ),
     );
   }
 }
