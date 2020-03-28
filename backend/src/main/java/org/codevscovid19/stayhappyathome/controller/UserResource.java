9d96f58bea9c9e84ea5420ad77995d1afc0b8c05
Jonas Alder <jonas.alder@gmail.com>
2020-03-28 15:39:59 +0100

    adapt feelings to new DTO shenanigans

--- backend/src/main/java/org/codevscovid19/stayhappyathome/controller/UserResource.java
+++ backend/src/main/java/org/codevscovid19/stayhappyathome/controller/UserResource.java
@@ -1,5 +1,7 @@
 package org.codevscovid19.stayhappyathome.controller;
 
+import org.codevscovid19.stayhappyathome.dto.FeelingDto;
+import org.codevscovid19.stayhappyathome.dto.UserDto;
 import org.codevscovid19.stayhappyathome.entity.Feeling;
 import org.codevscovid19.stayhappyathome.entity.User;
 import org.codevscovid19.stayhappyathome.repository.FeelingRepository;
@@ -7,7 +9,12 @@ import org.codevscovid19.stayhappyathome.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 import java.util.Optional;
@@ -17,42 +24,45 @@ import java.util.Set;
 @RequestMapping("/api/v1/user")
 public class UserResource {
 
-    private final UserRepository userRepository;
-    private final FeelingRepository feelingRepository;
+	private final UserRepository userRepository;
+	private final FeelingRepository feelingRepository;
 
-    @Autowired
-    public UserResource(UserRepository userRepository, FeelingRepository feelingRepository) {
-        this.userRepository = userRepository;
-        this.feelingRepository = feelingRepository;
-    }
+	@Autowired
+	public UserResource(UserRepository userRepository, FeelingRepository feelingRepository) {
+		this.userRepository = userRepository;
+		this.feelingRepository = feelingRepository;
+	}
 
-    @GetMapping(path = "/{id}", produces = "application/json")
-    public ResponseEntity<User> getUser(@PathVariable("id") String id) {
-        Optional<User> user = userRepository.findById(id);
-        return user.map(ResponseEntity::ok)
-                .orElseGet(() -> ResponseEntity.status(HttpStatus.NOT_FOUND).build());
-    }
+	@GetMapping(path = "/{id}", produces = "application/json")
+	public ResponseEntity<User> getUser(@PathVariable("id") String id) {
+		Optional<User> user = userRepository.findById(id);
+		return user.map(ResponseEntity::ok)
+				.orElseGet(() -> ResponseEntity.status(HttpStatus.NOT_FOUND).build());
+	}
 
-    @GetMapping(produces = "application/json")
-    public ResponseEntity<List<User>> getUsers() {
-        return ResponseEntity.ok(userRepository.findAll());
-    }
+	@GetMapping(produces = "application/json")
+	public ResponseEntity<List<User>> getUsers() {
+		return ResponseEntity.ok(userRepository.findAll());
+	}
 
-    @PostMapping(produces = "application/json")
-    public ResponseEntity<User> createUser(@RequestBody User user) {
-        return ResponseEntity.ok(userRepository.save(user));
-    }
+	@PostMapping(consumes = "application/json", produces = "application/json")
+	public ResponseEntity<User> createUser(@RequestBody UserDto userDto) {
+		User user = new User(userDto.getId(), userDto.getName(), userDto.getPhoto());
+		return ResponseEntity.ok(userRepository.save(user));
+	}
 
-    @PostMapping(path = "/{id}/feeling", consumes = "application/json", produces = "application/json")
-    public ResponseEntity<Feeling> createFeeling(@PathVariable String id, @RequestBody Feeling feeling) {
-        Optional<User> userEntity = userRepository.findById(id);
-        userEntity.ifPresent(user -> user.addFeeling(feeling));
-        return ResponseEntity.ok(feelingRepository.save(feeling));
-    }
+	@PostMapping(path = "/{id}/feeling", consumes = "application/json", produces = "application/json")
+	public ResponseEntity<Feeling> createFeeling(@PathVariable String id, @RequestBody FeelingDto feelingDto) {
+		Feeling feeling = new Feeling(feelingDto.getEmoji());
 
-    @GetMapping(path = "/{id}/feeling", produces = "application/json")
-    public ResponseEntity<Set<Feeling>> getFeeling(@PathVariable String id) {
-        Optional<User> userEntity = userRepository.findById(id);
-        return ResponseEntity.ok(userEntity.get().getFeelings());
-    }
+		Optional<User> userEntity = userRepository.findById(id);
+		userEntity.ifPresent(user -> user.addFeeling(feeling));
+		return ResponseEntity.ok(feelingRepository.save(feeling));
+	}
+
+	@GetMapping(path = "/{id}/feeling", produces = "application/json")
+	public ResponseEntity<Set<Feeling>> getFeeling(@PathVariable String id) {
+		Optional<User> userEntity = userRepository.findById(id);
+		return ResponseEntity.ok(userEntity.get().getFeelings());
+	}
 }
--- backend/src/main/java/org/codevscovid19/stayhappyathome/dto/FeelingDto.java
+++ backend/src/main/java/org/codevscovid19/stayhappyathome/dto/FeelingDto.java
@@ -1,23 +1,33 @@
 package org.codevscovid19.stayhappyathome.dto;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
-import javax.persistence.Table;
+import org.codevscovid19.stayhappyathome.entity.Emoji;
 
 public class FeelingDto {
 	private Long id;
+	private Emoji emoji;
 
 	private FeelingDto() {
 		// for Jackson
 	}
 
-	public FeelingDto(Long id) {
+	public FeelingDto(Long id, Emoji emoji) {
 		this.id = id;
+		this.emoji = emoji;
 	}
 
 	public Long getId() {
 		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public Emoji getEmoji() {
+		return emoji;
+	}
+
+	public void setEmoji(Emoji emoji) {
+		this.emoji = emoji;
 	}
 }
