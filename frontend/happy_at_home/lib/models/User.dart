8f05484e61bfa0f54c0a72fe38f4b971c998dd51
Bachmann, Benjamin <Benjamin.Bachmann@zuehlke.com>
2020-03-28 19:01:35 +0100

    Add post feelings and enum string converter.

--- frontend/happy_at_home/lib/models/User.dart
+++ frontend/happy_at_home/lib/models/User.dart
@@ -1,15 +1,19 @@
 import 'package:dart_json_mapper/dart_json_mapper.dart';
 import 'package:uuid/uuid.dart';
 
+import 'Feeling.dart';
+
 @jsonSerializable
 class User {
   final String id;
   final String name;
+  final String photo;
+  final Set<Feeling> feelingRecords;
 
-  User({this.id, this.name});
+  User({this.id, this.name, this.photo, this.feelingRecords});
 
   static User createUser(String name) {
     final id = Uuid().v4();
-    return User(id: id, name: name);
+    return User(id: id, name: name, photo: null, feelingRecords: {});
   }
 }
