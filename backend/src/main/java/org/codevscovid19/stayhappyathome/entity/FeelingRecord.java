51aa94e3eac64984762553b51c924b1811ce427a
Jonas Alder <jonas.alder@gmail.com>
2020-03-28 23:24:12 +0100

    fix import after merge

--- backend/src/main/java/org/codevscovid19/stayhappyathome/entity/FeelingRecord.java
+++ backend/src/main/java/org/codevscovid19/stayhappyathome/entity/FeelingRecord.java
@@ -7,6 +7,7 @@ import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.OneToMany;
+import javax.persistence.SequenceGenerator;
 import javax.persistence.Table;
 
 import java.time.LocalDateTime;
