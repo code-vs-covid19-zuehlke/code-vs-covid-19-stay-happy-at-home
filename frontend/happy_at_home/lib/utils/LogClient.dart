2d38c400b6f55963aa1510f613bfe14cd4f0c472
Bachmann, Benjamin <Benjamin.Bachmann@zuehlke.com>
2020-03-29 18:05:08 +0200

    Frontend: Log requests and responses.

--- frontend/happy_at_home/lib/utils/LogClient.dart
+++ frontend/happy_at_home/lib/utils/LogClient.dart
@@ -1,3 +1,5 @@
+import 'dart:convert';
+
 import 'package:http/http.dart';
 
 class LogClient extends BaseClient {
@@ -5,17 +7,80 @@ class LogClient extends BaseClient {
 
   final Client delegate;
 
+  @override
   Future<StreamedResponse> send(BaseRequest request) {
     _logRequest(request);
     return delegate.send(request);
+  }
+
+  @override
+  Future<Response> head(url, {Map<String, String> headers}) {
+    final response = super.head(url, headers: headers);
+    _logResponse(response);
+    return response;
+  }
+
+  @override
+  Future<Response> get(url, {Map<String, String> headers}) {
+    final response = super.get(url, headers: headers);
+    _logResponse(response);
+    return response;
+  }
+
+  @override
+  Future<Response> post(url, {Map<String, String> headers, body, Encoding encoding}) {
+    final response = super.post(url, headers: headers, body: body, encoding: encoding);
+    _logResponse(response);
+    return response;
+  }
+
+  @override
+  Future<Response> put(url, {Map<String, String> headers, body, Encoding encoding}) {
+    final response = super.put(url, headers: headers, body: body, encoding: encoding);
+    _logResponse(response);
+    return response;
+  }
+
+  @override
+  Future<Response> patch(url, {Map<String, String> headers, body, Encoding encoding}) {
+    final response = super.patch(url, headers: headers, body: body, encoding: encoding);
+    _logResponse(response);
+    return response;
+  }
+
+  @override
+  Future<Response> delete(url, {Map<String, String> headers}) {
+    final response = super.delete(url, headers: headers);
+    _logResponse(response);
+    return response;
+  }
+
+  @override
+  Future<String> read(url, {Map<String, String> headers}) {
+    final response = super.read(url, headers: headers);
+    _logResponseString(response);
+    return response;
   }
 
   void close() => delegate.close();
 
   void _logRequest(BaseRequest request) {
     print('Request (${request.method}) URL: ${request.url}\nHeaders: ${request.headers}');
-    if (request is Request && request.body!= null) {
+    if (request is Request && request.body != null && request.body.length > 0) {
       print('Body: ${request.body}');
     }
+  }
+
+  void _logResponse(Future<Response> futureResponse) async {
+    final response = await futureResponse;
+    print('Response (${response.statusCode}): Headers: ${response.headers}');
+    if (response.body != null && response.body.length > 0) {
+      print('Body: ${ response.body}');
+    }
+  }
+
+  void _logResponseString(Future<String> futureResponse) async {
+    final response = await futureResponse;
+    print('Response string: $response');
   }
 }
