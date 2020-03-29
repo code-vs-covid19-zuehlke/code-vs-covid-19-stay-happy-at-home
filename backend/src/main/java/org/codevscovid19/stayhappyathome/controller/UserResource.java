cdd11a078903b44dff7331cc6a202efa18e06838
Savrim, Tuana <tuana.savrim@zuehlke.com>
2020-03-29 17:33:14 +0200

    Add feelings resource

--- backend/src/main/java/org/codevscovid19/stayhappyathome/controller/UserResource.java
+++ backend/src/main/java/org/codevscovid19/stayhappyathome/controller/UserResource.java
@@ -1,49 +1,32 @@
 package org.codevscovid19.stayhappyathome.controller;
 
-import org.codevscovid19.stayhappyathome.dto.FeelingDto;
 import org.codevscovid19.stayhappyathome.dto.TimeRecordDto;
 import org.codevscovid19.stayhappyathome.dto.UserDto;
-import org.codevscovid19.stayhappyathome.entity.Feeling;
-import org.codevscovid19.stayhappyathome.entity.FeelingRecord;
 import org.codevscovid19.stayhappyathome.entity.TimeRecord;
 import org.codevscovid19.stayhappyathome.entity.User;
 import org.codevscovid19.stayhappyathome.login.HansNotFoundException;
-import org.codevscovid19.stayhappyathome.repository.FeelingRepository;
 import org.codevscovid19.stayhappyathome.repository.UserRepository;
 import org.codevscovid19.stayhappyathome.service.PhotoService;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.io.IOException;
 import java.net.URL;
 import java.util.List;
 import java.util.Optional;
-import java.util.stream.Collectors;
 
 @RestController
 @RequestMapping("/api/v1/user")
 public class UserResource {
 
-  private static final Logger logger = LoggerFactory.getLogger(UserResource.class);
-
   private final UserRepository userRepository;
-  private final FeelingRepository feelingRepository;
   private final PhotoService photoService;
 
   @Autowired
-  public UserResource(UserRepository userRepository, FeelingRepository feelingRepository, PhotoService photoService) {
+  public UserResource(UserRepository userRepository, PhotoService photoService) {
     this.userRepository = userRepository;
-    this.feelingRepository = feelingRepository;
     this.photoService = photoService;
   }
 
@@ -76,24 +59,4 @@ public class UserResource {
     return ResponseEntity.ok().build();
   }
 
-  @PutMapping(path = "/{id}/feeling", consumes = "application/json", produces = "application/json")
-  public ResponseEntity<Void> addFeelings(@PathVariable String id, @RequestBody List<FeelingDto> feelingDtos) {
-    User user = userRepository.findById(id).orElseThrow(() -> new HansNotFoundException("User", id));
-
-    List<Feeling> feelings = feelingDtos.stream()
-      .map(feelingDto -> new Feeling(feelingDto.getEmoji()))
-      .collect(Collectors.toList());
-    FeelingRecord feelingRecord = new FeelingRecord(feelings);
-    user.addFeelings(feelingRecord);
-    User saved = userRepository.save(user);
-    logger.debug("user after save: " + saved);
-    return ResponseEntity.ok().build();
-  }
-
-  @GetMapping(path = "/{id}/feeling", produces = "application/json")
-  public ResponseEntity<List<Feeling>> getFeelings(@PathVariable String id) {
-    Optional<User> userEntity = userRepository.findById(id);
-    List<Feeling> latestFeelings = userEntity.get().getFeelings();
-    return ResponseEntity.ok(latestFeelings);
-  }
 }
